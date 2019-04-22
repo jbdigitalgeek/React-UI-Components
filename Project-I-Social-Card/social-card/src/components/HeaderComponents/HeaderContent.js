de08df910b49c96333ff69bb2da2b7807d2a84d1
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-22 13:59:55 -0700

    first commit initial components being built

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+        <div className="header-content">
+            <p></p>
+        </div>
+    );
+};
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderTitle = () => {
+    return (
+        <div className="Header-Title">
+            <ImageThumbnail />
+            <p>@LambdaSchool<time datetime="2019-01-26"></time></p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
