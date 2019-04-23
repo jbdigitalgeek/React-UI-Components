463e48957f5ad54cfc0aa29aa5b11e05f947851a
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-23 15:28:19 -0700

    components done on calculator starting styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './App.scss';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import './components/ButtonComponents/Button.scss'
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="container">
+      <CalculatorDisplay />
+      <div className="row-1">
+        <ActionButton className="clear-contents" text="clear" />
+        <NumberButton buttonStyle="mod-bg" text="÷" /> 
+      </div>
+      <div className="row-2">
+        <NumberButton buttonStyle="" text="7" />
+        <NumberButton buttonStyle="" text="8" />
+        <NumberButton buttonStyle="" text="9" />
+        <NumberButton buttonStyle="" text="x" />
+      </div>
+      <div className="row-3">
+        <NumberButton buttonStyle="" text="4" />
+        <NumberButton buttonStyle="" text="5" />
+        <NumberButton buttonStyle="" text="6" />
+        <NumberButton buttonStyle="" text="-" />
+      </div>
+      <div className="row-4">
+        <NumberButton buttonStyle="" text="1" />
+        <NumberButton buttonStyle="" text="2" />
+        <NumberButton buttonStyle="" text="3" />
+        <NumberButton buttonStyle="" text="+" />
+      </div>
+      <div className="bottom-row">
+        <NumberButton buttonStyle="" text="0" />
+        <NumberButton buttonStyle="" text="=" />
+      </div>
+    
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+const ActionButton = props => {
+    return (
+        <button className={`big-button ${props.className}`}>{props.text}
+        </button>
+    );
+};
+
+export default ActionButton;
